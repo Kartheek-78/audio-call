4cb931469a7a73e542df31588ba300b18a7e4099
TATIPARTHI KARTHEEK REDDY <137144995+Kartheek-78@users.noreply.github.com>
2025-09-28 15:31:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import eventlet
-eventlet.monkey_patch()
-
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
@@ -18,4 +15,5 @@ def handle_signal(message):
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
+
 
